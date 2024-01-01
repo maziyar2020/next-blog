7de9ab07fda0b04b945245aac07984c12ef0eb18
maziyar <maziroadtodivine@gmail.com>
2024-01-01 22:45:09 +0330

    Change blog sort logic base on current query.

--- src/components/blogs/SortBar.jsx
+++ src/components/blogs/SortBar.jsx
@@ -2,27 +2,24 @@
 import { AdjustmentsIcon } from '@heroicons/react/outline'
 // router
 import { useRouter } from 'next/router'
-// hooks
-import { useState } from 'react'
+// redux
+import { useDispatch, useSelector } from 'react-redux'
+import { setPageQuery } from 'src/redux/page/pageActions'
 
-const sortbarOptions = [
-    { label: 'محبوب ترین', id: 'popular' },
-    { label: 'پر بازدیدترین', id: 'most' },
-    { label: 'جدید ترین', id: 'newest' },
-]
 
 const SortBar = () => {
     const router = useRouter()
-    const [sort, setSort] = useState(router.query.sort || 'popular')
+    const pageData = useSelector(state => state.pageData)
+    const { blogQuery, selectedQuery } = pageData
+    const dispatch = useDispatch()
 
     const sortHandler = (id) => {
-
-        setSort(id)
-
+        dispatch(setPageQuery(id))
         router.replace({
             query: { ...router.query, sort: id },
         });
-
+        
+        console.log(selectedQuery);
     }
 
     return (
@@ -33,16 +30,16 @@ const SortBar = () => {
             </div>
             <ul className="flex items-center justify-between gap-x-2">
                 {
-                    sortbarOptions.map((item, index) => {
+                    blogQuery.map((item, index) => {
                         return <li
                             className={`px-1 md:px-3 py-4 sm:text-normal md:text-md text-white relative cursor-pointer
-                                ${item.id === sort ? 'text-[] text-md' : ''}
+                                ${item.name === selectedQuery ? 'text-[] text-md' : `${item.name}`}
                             `}
                             key={index}
-                            onClick={() => { sortHandler(item.id) }}
+                            onClick={() => { sortHandler(item.name) }}
                         >
                             {item.label}
-                            {item.id === sort &&
+                            {item.name === selectedQuery &&
                                 (<span className="absolute bottom-0 right-1 h-[3px] w-[75px] md:w-[95px] bg-[#ffc107]"></span>)
                             }
                         </li>
